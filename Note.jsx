f21fb243d9672cc18341b268e1ccee29caa6fb57
SKM162 <51273744+SKM162@users.noreply.github.com>
2021-06-05 17:33:37 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>HTML</h1>
+      <p>
+        This forms the basic skeleton for our webpage. We can maintain the DOM
+        of our page by using this. Other libraries are linked here.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
